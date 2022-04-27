3b2c54074f7a6e5583a05fd53e3c49acf77e2dbe
Kevin Sana <kevin.sana@outlook.com>
2022-04-27 14:08:43 -0500

    created api component and got functionality for rendering api data into a list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import './app.styles.scss';
+
+
+
+const ApiComponent = ({ users, setUsers }) => {
+
+  // API request is made when this component renders
+  useEffect(() => {
+    Axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        // sets the API data to our State defined in the App Component
+        setUsers(res.data);
+      });
+  }, [setUsers]);
+
+  return (
+    // Renders the API Data in their own Component
+    <>
+      <h1>API Component</h1>
+      <input
+        type="text"
+        onChange={(event) => {
+          const searchString = event.target.value.toLowerCase();
+          const newArray = users.filter((user) => {
+            return user.name.toLowerCase().includes(searchString);
+          });
+          setUsers(() => {
+            return newArray
+          })
+        }} />
+        {/* Here we map through the users state (defined in the App Component)
+            with each user in the state, will render a div with its own user.id for key
+            and li with user.name
+         */}
+      <ul>
+        {users.map((user) => {
+          return (
+            <div key={user.id}>
+              <li>{user.name}</li>
+            </div>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+
   return (
     <div>
-      <h1>APP</h1>
+      <h1>App</h1>
+      <ApiComponent users={users} setUsers={setUsers} />
     </div>
   );
 }
